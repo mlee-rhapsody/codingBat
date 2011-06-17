7ff7f338c5cbb14350a95afae5551fbcf93d2077
Miene Lee <mlee@rhapsody.com>
2011-06-16 18:11:17 -0700

    Completed last2 (horrible problem wording)

--- src/javalearning/Warmup2.java
+++ src/javalearning/Warmup2.java
@@ -26,4 +26,56 @@ public class Warmup2 {
 			
 		return base;
 	}
+
+	public static String stringBits(String str) {
+		String base = "";
+		
+		if(str.length()==0)
+			return "";
+		
+		for(int i=0; i<str.length(); i+=2){
+			base = base + str.charAt(i);
+		}
+		return base;
+	}
+	
+	public static String stringSplosion(String str){
+		if(str.length()==0)
+			return str;
+		
+		String base = "";
+		for(int i=0; i<str.length(); i++){
+			base = (str.substring(0, str.length()-i)) + base;
+		}
+		
+		return base;
+	}
+	
+	public static int last2(String str){
+		if(str.length()<2)
+			return 0;
+		
+		
+		String searchTarget = str.substring(str.length()-2).trim();
+		String preSubstring = str.substring(0, str.length()-2);
+
+		if(str.length()==4){
+			String preString = str.substring(0, 2);
+			if(preString.equals(searchTarget))
+				return 2;
+		}
+		
+		int count=0;
+		for(int i=0; i<=preSubstring.length()-2; i++){
+			String substring = preSubstring.substring(i, i+2);
+			if(substring.equals(searchTarget))
+				count++;
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Hello world: "+Warmup2.last2("h"));
+	}
 }
--- test/javalearning/Warmup2Tests.java
+++ test/javalearning/Warmup2Tests.java
@@ -2,9 +2,32 @@ package javalearning;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Warmup2Tests {
+	
+	@Test
+	public void last2(){
+		assertEquals(1, Warmup2.last2("hixxhi"));
+		assertEquals(1, Warmup2.last2("xaxxaxaxx"));
+		assertEquals(2, Warmup2.last2("axxxaaxx"));
+	}
+	
+	@Test
+	public void stringSplosion(){
+		assertEquals("CCoCodCode", Warmup2.stringSplosion("Code"));
+		assertEquals("aababc", Warmup2.stringSplosion("abc"));
+		assertEquals("aab", Warmup2.stringSplosion("ab"));
+	}
+	
+	@Test
+	public void stringBits(){
+		
+		assertEquals("Hlo", Warmup2.stringBits("Hello"));
+		assertEquals("H", Warmup2.stringBits("Hi"));
+		assertEquals("Hello", Warmup2.stringBits("Heeololeo"));
+	}
 	
 	@Test
 	public void frontTimes(){
