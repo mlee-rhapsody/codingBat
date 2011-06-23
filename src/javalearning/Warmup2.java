d5ea9c982761e1ba8bba4d840cf3197fc0cefa6b
Miene Lee <mlee@rhapsody.com>
2011-06-22 17:19:53 -0700

    More warmup2

--- src/javalearning/Warmup2.java
+++ src/javalearning/Warmup2.java
@@ -1,14 +1,87 @@
 package javalearning;
 
 public class Warmup2 {
-	public static String stringTimes(String str, int n){
+	
+	public static String altPairs(String str){
 		
-		String base = "";
-		for(int i=0; i<n; i++){
-			base += str;
+		
+		return "";
+	}
+	
+	public static String stringX(String str){
+		if(str.isEmpty())
+			return "";
+		
+		if(str.length()<2)
+			return str;
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append(str.charAt(0));
+		for(int i=1; i< str.length()-1; i++){
+			if(str.toLowerCase().charAt(i) != 'x')
+				sb.append(str.charAt(i));
 		}
 		
-		return base;
+		sb.append(str.charAt(str.length()-1));
+		
+		return sb.toString();
+	}
+	
+	public static int stringMatch(String a, String b){
+		if(a.isEmpty() || b.isEmpty())
+			return 0;
+		int shortLength = a.length();
+		
+		if(b.length() < shortLength)
+			shortLength = b.length();
+		
+		int tokenCount = 0;
+		for(int i=0; i< shortLength-1; i++){
+			if(a.charAt(i) == b.charAt(i) && a.charAt(i+1)==b.charAt(i+1)  )
+				tokenCount++;
+		}
+		
+		return tokenCount;
+	}
+	
+	public static boolean array123(int[] nums){
+		if(nums.length<3)
+			return false;
+		
+		int i =0;
+		do{
+			boolean test=true;
+			if(nums[i]!=1) test = false;
+			if(nums[i+1]!=2) test = false;
+			if(nums[i+2]!=3) test = false;
+			
+			if(test == true)
+				return true;
+			
+
+		}while(i++<nums.length-3);
+		
+		return false;
+	}
+	public static boolean arrayFront9(int[] nums){
+		int maxCount=nums.length;
+		if(nums.length>4)
+			maxCount=4;
+		
+		for(int i=0; i<maxCount; i++){
+			if(nums[i]==9)
+				return true;
+		}
+		
+		return false;
+	}
+	public static int arrayCount9(int[] nums){
+		int count=0;
+		for(int i=0; i<nums.length; i++){
+			if(nums[i]==9)
+				count++;
+		}
+		return count;
 	}
 	
 	public static String frontTimes(String str, int n){
@@ -24,30 +97,6 @@ public class Warmup2 {
 			base += tmp;
 		}
 			
-		return base;
-	}
-
-	public static String stringBits(String str) {
-		String base = "";
-		
-		if(str.length()==0)
-			return "";
-		
-		for(int i=0; i<str.length(); i+=2){
-			base = base + str.charAt(i);
-		}
-		return base;
-	}
-	
-	public static String stringSplosion(String str){
-		if(str.length()==0)
-			return str;
-		
-		String base = "";
-		for(int i=0; i<str.length(); i++){
-			base = (str.substring(0, str.length()-i)) + base;
-		}
-		
 		return base;
 	}
 	
@@ -74,8 +123,42 @@ public class Warmup2 {
 		
 		return count;
 	}
-	
+
 	public static void main(String[] args){
 		System.out.println("Hello world: "+Warmup2.last2("h"));
+	}
+	
+	public static String stringBits(String str) {
+		String base = "";
+		
+		if(str.length()==0)
+			return "";
+		
+		for(int i=0; i<str.length(); i+=2){
+			base = base + str.charAt(i);
+		}
+		return base;
+	}
+	
+	public static String stringSplosion(String str){
+		if(str.length()==0)
+			return str;
+		
+		String base = "";
+		for(int i=0; i<str.length(); i++){
+			base = (str.substring(0, str.length()-i)) + base;
+		}
+		
+		return base;
+	}
+	
+	public static String stringTimes(String str, int n){
+		
+		String base = "";
+		for(int i=0; i<n; i++){
+			base += str;
+		}
+		
+		return base;
 	}
 }
--- test/javalearning/Warmup2Tests.java
+++ test/javalearning/Warmup2Tests.java
@@ -8,17 +8,65 @@ import org.junit.Test;
 public class Warmup2Tests {
 	
 	@Test
+	public void stringX(){
+		assertEquals("x", Warmup2.stringX("x"));
+		assertEquals("xx", Warmup2.stringX("xx"));
+		assertEquals("xHix", Warmup2.stringX("xxHxix"));
+		assertEquals("abcd", Warmup2.stringX("abxxxcd"));
+		assertEquals("", Warmup2.stringX(""));
+		assertEquals("xabcdx", Warmup2.stringX("xabxxxcdx"));
+	}
+	
+	@Test
+	public void stringMatch(){
+		assertEquals(3, Warmup2.stringMatch("xxcaazz", "xxbaaz"));
+		assertEquals(2, Warmup2.stringMatch("abc", "abc"));
+		assertEquals(0, Warmup2.stringMatch("abc", "axc"));
+		assertEquals(0, Warmup2.stringMatch("", ""));
+		assertEquals(0, Warmup2.stringMatch("a", "a"));
+	}
+	
+	
+	@Test
+	public void array123(){
+		int[] num1 = {1,1,2,3,1};
+		int[] num2 = {1,1,2,4,1};
+		int[] num3 = {1,2,3};
+		int[] num4 = {1, 1, 2, 1, 2, 3};
+		
+		assertEquals(true, Warmup2.array123(num4));
+		assertEquals(true, Warmup2.array123(num3));
+		assertEquals(true, Warmup2.array123(num1));
+		assertEquals(false, Warmup2.array123(num2));
+	}
+	
+	@Test
+	public void arrayFront9(){
+		int[] num1 = {1,2,9,3,4};
+		int[] num2 = {1,2,3,4,9};
+		int[] num3 = {1,2,3,4,5};
+		
+		assertEquals(true, Warmup2.arrayFront9(num1));
+		assertEquals(false, Warmup2.arrayFront9(num2));
+		assertEquals(false, Warmup2.arrayFront9(num3));
+	}
+	@Test
+	public void arrayCount9(){
+		int[] nums = {1,2,9};
+		assertEquals(1, Warmup2.arrayCount9(nums));
+	}
+	@Test
+	public void frontTimes(){
+		assertEquals("ChoCho", Warmup2.frontTimes("Chocolate", 2));
+		assertEquals("ChoChoCho", Warmup2.frontTimes("Chocolate", 3));
+		assertEquals("AbcAbcAbc", Warmup2.frontTimes("Abc", 3));
+	}
+	
+	@Test
 	public void last2(){
 		assertEquals(1, Warmup2.last2("hixxhi"));
 		assertEquals(1, Warmup2.last2("xaxxaxaxx"));
 		assertEquals(2, Warmup2.last2("axxxaaxx"));
-	}
-	
-	@Test
-	public void stringSplosion(){
-		assertEquals("CCoCodCode", Warmup2.stringSplosion("Code"));
-		assertEquals("aababc", Warmup2.stringSplosion("abc"));
-		assertEquals("aab", Warmup2.stringSplosion("ab"));
 	}
 	
 	@Test
@@ -30,11 +78,10 @@ public class Warmup2Tests {
 	}
 	
 	@Test
-	public void frontTimes(){
-		assertEquals("ChoCho", Warmup2.frontTimes("Chocolate", 2));
-		assertEquals("ChoChoCho", Warmup2.frontTimes("Chocolate", 3));
-		assertEquals("AbcAbcAbc", Warmup2.frontTimes("Abc", 3));
-		
+	public void stringSplosion(){
+		assertEquals("CCoCodCode", Warmup2.stringSplosion("Code"));
+		assertEquals("aababc", Warmup2.stringSplosion("abc"));
+		assertEquals("aab", Warmup2.stringSplosion("ab"));
 	}
 	
 	@Test
