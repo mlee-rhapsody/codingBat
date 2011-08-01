df290d34e02c6e0d2a6434ae428dc48498fb310a
Miene Lee <mlee@rhapsody.com>
2011-07-31 18:19:31 -0700

    Finished String2

--- src/javalearning/String2.java
+++ src/javalearning/String2.java
@@ -1,82 +1,493 @@
 package javalearning;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class String2 {
-	
+
 	public static final String2 INSTANCE = new String2();
-	
+
 	private String2() {
 	}
-	
+
 	public static void main(String[] args) {
 		String2 tst = String2.INSTANCE;
-		
+
 		print(tst.countCode("cozexxcope"));
 	}
-	
+
 	@SuppressWarnings("unused")
 	private static void print(boolean catDog) {
 		System.out.println(catDog);
-		
+
 	}
 
 	private static void print(int countHi) {
 		System.out.println(countHi);
 	}
-	//////////////////////////
+
+	// ////////////////////////
 	
-	public int countCode(String str){
+	public String wordEnds(String str, String word){
+		if(str.equals(word))
+			return "";
 		
-		int codeCount = 0;
-		
-		for(int i=0; i<str.length()-3; i++){
-			String substring = str.substring(i, i+4);
-			System.out.printf("%2d %s%n", i, substring);
-			if(substring.startsWith("co")&&substring.endsWith("e") )
-				codeCount++;
+		if(str.startsWith(word) && str.endsWith(word)){
+			int delta = str.length()-word.length()-word.length();
+			
+			if(delta==0){
+				String begWord = word.substring(0, 1);
+				String endWord = word.substring(word.length()-1);
+				
+				return begWord + endWord;
+			}
+			
+			
+			if(delta==1){
+				String newToken = str.substring(word.length(), str.length()-word.length());
+				return newToken+newToken;
+			}
+			
+			if(delta >2){
+				String newToken = str.substring(word.length(), str.length()-word.length());
+
+				if(!newToken.contains(word)){
+					String beg=newToken.charAt(0)+"";
+					String end = newToken.charAt(newToken.length()-1)+"";
+					
+					return beg+end;
+				}
+			}
 		}
 		
+		int wordLength = word.length();
+		ArrayList<Integer> wordList = new ArrayList<Integer>();
+		for(int i=0; i<str.length()-wordLength; i++){
+			String substring = str.substring(i, i+wordLength);
+			if(substring.contains(word)){
+				wordList.add(i);
+			}
+		}
 		
-		return codeCount;
+		StringBuffer sb = new StringBuffer();
+		for(int element: wordList){
+			String left = "";
+			String right = "";
+			
+			if(element>0){
+				left = str.charAt(element-1)+"";
+				sb.append(left);
+			}
+			
+			if(element+wordLength<str.length()){
+				right = str.charAt(element+wordLength)+"";
+				sb.append(right);
+			}
+		}
+		
+		if(str.endsWith(word)){
+			String endLetter = str.charAt(str.length()-word.length()-1)+"";
+			sb.append(endLetter);
+		}
+		
+		return sb.toString();
 	}
 	
-	public boolean catDog(String str){
+	public String plusOut(String str, String word){
+		if(str.isEmpty())
+			return "";
 		
-		int dogCount = 0;
-		int catCount = 0;
-		for(int i=0; i<str.length()-2; i++){
-			String substring = str.substring(i, i+3);
-			if(substring.equals("dog"))
-				dogCount++;
-			if(substring.equals("cat"))
-				catCount++;
+		int wordLength = word.length();
+		ArrayList<Integer> wordList = new ArrayList<Integer>();
+		for(int i=0; i<str.length()-wordLength; i++){
+			String substring = str.substring(i, i+wordLength);
+			if(substring.contains(word)){
+				wordList.add(i);
+			}
 		}
 		
+		char[] plusArray = str.toCharArray();
+		for(int i=0; i<plusArray.length; i++)
+			plusArray[i]='+';
+		
+		for(int element: wordList){
+			int count=0;
+			for(int i=element; i<(element+wordLength); i++){
+				plusArray[i] = word.charAt(count);
+				count++;
+			}
+		}
+		
+		if(str.endsWith(word)){
+			int count = 0;
+			for(int i=str.length()-wordLength; i<str.length(); i++){
+				plusArray[i] = word.charAt(count);
+				count++;
+			}
+		}
+		return String.valueOf(plusArray);
+	}
+	
+	public String starOut(String str) {
+		if (str.isEmpty())
+			return "";
+
+		if (str.contains("*") == false)
+			return str;
+
+		if (str.length()<3 && str.contains("*"))
+			return "";
+
+		List<Integer> starList = new ArrayList<Integer>();
+
+		for (int i = 1; i < str.length() - 1; i++) {
+			String letter = str.charAt(i) + "";
+			if (letter.equals("*")) {
+				starList.add(i);
+			}
+		}
+
+		char[] strArray = str.toCharArray();
+		
+		String startChar = str.charAt(0)+"";
+		String endChar = str.substring(str.length()-1);
+		
+		if(startChar.contains("*"))
+			strArray[1]='*';
+		
+		if(endChar.contains("*"))
+			strArray[str.length()-2]='*';
+		
+		for (int element : starList) {
+			strArray[element - 1] = '*';
+			strArray[element + 1] = '*';
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		for(int i =0; i< strArray.length; i++){
+			String letter = strArray[i] + "";
+			if(!letter.equals("*"))
+				sb.append(letter);
+		}
+
+		return sb.toString();
+	}
+
+	public String zipZap(String str) {
+
+		if (str.length() < 3)
+			return str;
+
+		boolean ignoreAppend = false;
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < str.length() - 2; i++) {
+			String substring = str.substring(i, i + 3);
+
+			String z = substring.charAt(0) + "";
+			String p = substring.charAt(2) + "";
+
+			if (z.equals("z") && p.equals("p")) {
+				sb.append("zp");
+
+				if (i == str.length() - 3)
+					ignoreAppend = true;
+
+				i = i + 2;
+			} else {
+				sb.append(str.charAt(i) + "");
+			}
+		}
+		if (ignoreAppend == false) {
+			sb.append(str.charAt(str.length() - 2) + "");
+			sb.append(str.charAt(str.length() - 1) + "");
+		}
+
+		return sb.toString();
+	}
+
+	public boolean sameStarChar(String str) {
+
+		if (str.isEmpty())
+			return true;
+
+		if (str.length() == 1 && str.equals("*"))
+			return true;
+
+		if (str.length() == 2 && str.contains("*"))
+			return true;
+
+		if (str.contains("*") == false)
+			return true;
+
+		boolean sentinel = false;
+
+		for (int i = 1; i < str.length() - 1; i++) {
+			String letter = str.charAt(i) + "";
+			String beforeLetter = str.charAt(i - 1) + "";
+			String afterLetter = str.charAt(i + 1) + "";
+
+			if (letter.equals("*") && beforeLetter.equals(afterLetter))
+				sentinel = true;
+
+			if (letter.equals("*") && beforeLetter.equals(afterLetter) == false) {
+				return false;
+			}
+		}
+
+		return sentinel;
+	}
+
+	public String getSandwich(String str) {
+		if (str.isEmpty() || !str.contains("bread"))
+			return "";
+
+		if (str.indexOf("bread") == str.lastIndexOf("bread"))
+			return "";
+		return str
+				.substring(str.indexOf("bread") + 5, str.lastIndexOf("bread"));
+	}
+
+	public boolean xyzMiddle(String str) {
+		if (str.isEmpty() || !str.contains("xyz"))
+			return false;
+
+		if (str.equals("xyz"))
+			return true;
+
+		if (str.length() == 4 && str.contains("xyz"))
+			return true;
+
+		if (str.length() < 4 && !str.contains("xyz"))
+			return false;
+
+		for (int i = 0; i < str.length() - 3; i++) {
+			String substring = str.substring(i, i + 3);
+			if (substring.equals("xyz")) {
+				String leftString = str.substring(0, i);
+				String rightString = str.substring(i + 3);
+
+				if (Math.abs(leftString.length() - rightString.length()) <= 1)
+					return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean prefixAgain(String str, int n) {
+
+		String prefix = str.substring(0, n);
+		String post = str.substring(n);
+		return post.contains(prefix);
+	}
+
+	public String repeatSeparator(String word, String sep, int count) {
+		if (count == 0)
+			return "";
+
+		if (count == 1)
+			return word;
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < count - 1; i++) {
+			sb.append(word);
+			sb.append(sep);
+		}
+
+		sb.append(word);
+
+		return sb.toString();
+	}
+
+	public String repeatFront(String str, int n) {
+
+		if (str.isEmpty())
+			return "";
+
+		if (str.length() == 1)
+			return str;
+
+		String substring = str.substring(0, n);
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(substring);
+
+		for (int i = n - 1; i >= 1; i--) {
+			String substring2 = substring.substring(0, i);
+			sb.append(substring2);
+		}
+
+		return sb.toString();
+	}
+
+	public String repeatEnd(String str, int n) {
+		if (str.isEmpty())
+			return "";
+
+		String end = str.substring(str.length() - n);
+
+		String repeat = "";
+		for (int i = 0; i < n; i++) {
+			repeat += end;
+		}
+		return repeat;
+	}
+
+	public String mixString(String a, String b) {
+
+		if (a.isEmpty() && b.isEmpty())
+			return "";
+
+		if (a.isEmpty() && b.length() > 0)
+			return b;
+
+		if (a.length() > 0 && b.isEmpty())
+			return a;
+
+		StringBuffer sb = new StringBuffer();
+		int maxLength = a.length() > b.length() ? a.length() : b.length();
+		for (int i = 0; i < maxLength; i++) {
+
+			if (i < a.length())
+				sb.append(a.charAt(i) + "");
+
+			if (i < b.length())
+				sb.append(b.charAt(i) + "");
+		}
+
+		return sb.toString();
+	}
+
+	public boolean xyBalance(String str) {
+
+		if (str.length() == 0)
+			return true;
+
+		if (str.length() == 1) {
+
+			String firstChar = str.charAt(0) + "";
+
+			if (firstChar.equals("y"))
+				return true;
+
+			if (firstChar.equals("x"))
+				return false;
+
+			return true;
+		}
+
+		if (str.contains("x") == false)
+			return true;
+
+		if (str.contains("y") == true && str.contains("x") == false)
+			return true;
+
+		if (str.contains("x") && str.contains("y")) {
+
+			int lastIndexOfX = str.lastIndexOf("x");
+			int lastIndexOfY = str.lastIndexOf("y");
+
+			if (lastIndexOfY > lastIndexOfX)
+				return true;
+		}
+		return false;
+	}
+
+	public boolean bobThere(String str) {
+		if (str.length() < 3)
+			return false;
+
+		if (str.contains("bob"))
+			return true;
+
+		for (int i = 0; i < str.length() - 2; i++) {
+			String substring = str.substring(i, i + 3);
+
+			String b1 = substring.charAt(0) + "";
+			String b2 = substring.charAt(2) + "";
+			if (b1.equals("b") && b2.equals("b"))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean xyzThere(String str) {
+		boolean sentinel = false;
+
+		if (str.startsWith("xyz"))
+			sentinel = true;
+
+		for (int i = 1; i < str.length() - 2; i++) {
+			String substring = str.substring(i, i + 3);
+			String prevI = "" + str.charAt(i - 1);
+
+			if (substring.equals("xyz") && !prevI.equals(".")) {
+				sentinel = true;
+				break;
+			}
+		}
+
+		return sentinel;
+	}
+
+	public boolean endOther(String a, String b) {
+
+		if (b.toLowerCase().endsWith(a.toLowerCase()))
+			return true;
+
+		if (a.toLowerCase().endsWith(b.toLowerCase()))
+			return true;
+
+		return false;
+	}
+
+	public int countCode(String str) {
+		int codeCount = 0;
+
+		for (int i = 0; i < str.length() - 3; i++) {
+			String substring = str.substring(i, i + 4);
+			if (substring.startsWith("co") && substring.endsWith("e"))
+				codeCount++;
+		}
+
+		return codeCount;
+	}
+
+	public boolean catDog(String str) {
+
+		int dogCount = 0;
+		int catCount = 0;
+		for (int i = 0; i < str.length() - 2; i++) {
+			String substring = str.substring(i, i + 3);
+			if (substring.equals("dog"))
+				dogCount++;
+			if (substring.equals("cat"))
+				catCount++;
+		}
+
 		return dogCount == catCount;
 	}
 
-	
-	
-	public int countHi(String str){
+	public int countHi(String str) {
 		int hiCount = 0;
-		for(int i=0; i<str.length()-1; i++){
-			String substring = str.substring(i, i+2);
-			if(substring.equals("hi"))
+		for (int i = 0; i < str.length() - 1; i++) {
+			String substring = str.substring(i, i + 2);
+			if (substring.equals("hi"))
 				hiCount++;
 		}
 		return hiCount;
 	}
-	
-	
-	public String doubleChar(String str){
+
+	public String doubleChar(String str) {
 		String tmp = "";
-		
-		for(int i=0; i<str.length(); i++){
-			 //String item = Character.toString(str.charAt(i));
-			 tmp += ""+str.charAt(i)+str.charAt(i);
+
+		for (int i = 0; i < str.length(); i++) {
+			// String item = Character.toString(str.charAt(i));
+			tmp += "" + str.charAt(i) + str.charAt(i);
 		}
-		
+
 		return tmp;
 	}
-	
-	
+
 }
--- test/javalearning/String2Tests.java
+++ test/javalearning/String2Tests.java
@@ -13,6 +13,203 @@ public class String2Tests {
 	}
 	
 	@Test
+	public void wordEnds(){
+		
+		assertEquals("acac", test.wordEnds("abc1xyz1abc", "b"));
+		
+		assertEquals("cxz", test.wordEnds("abc1xyz1", "1"));
+		assertEquals("cxziij", test.wordEnds("abc1xyz1i1j", "1"));
+		assertEquals("XY", test.wordEnds("XYXY", "XY"));
+		assertEquals("", test.wordEnds("foo", "foo"));
+		assertEquals("c13i", test.wordEnds("abcXY123XYijk", "XY"));
+		assertEquals("13", test.wordEnds("XY123XY", "XY"));
+		assertEquals("11", test.wordEnds("XY1XY", "XY"));
+	}
+	
+
+	@Test
+	public void plusOut(){
+		assertEquals("", test.plusOut("", "xy"));
+		assertEquals("++xy++", test.plusOut("12xy34", "xy"));
+		assertEquals("1+++++", test.plusOut("12xy34", "1"));
+		
+		assertEquals("++xy++xy+++xy", test.plusOut("12xy34xyabcxy", "xy"));
+		
+	}
+	
+	
+	@Test
+	public void starOut(){
+		assertEquals("", test.starOut(""));
+		assertEquals("nostar4u", test.starOut("nostar4u"));
+		
+		assertEquals("", test.starOut("a*"));
+		assertEquals("", test.starOut("*a"));
+		
+		assertEquals("", test.starOut("a*a"));
+		
+		assertEquals("ad", test.starOut("ab*cd"));
+		assertEquals("ad", test.starOut("ab**cd"));
+		assertEquals("silly", test.starOut("sm*eilly"));
+		assertEquals("string", test.starOut("stringy*"));
+		assertEquals("tring", test.starOut("*stringy*"));
+	}
+	
+	
+	@Test
+	public void zipZap(){
+		
+		assertEquals("foobar", test.zipZap("foobar"));
+		
+		assertEquals("zpXzp", test.zipZap("zipXzap"));
+		assertEquals("zpzp", test.zipZap("zopzop"));
+		assertEquals("zzzpzp", test.zipZap("zzzopzop"));
+	}
+	
+	@Test
+	public void sameStarChar(){
+		assertEquals(true, test.sameStarChar(""));
+		
+		assertEquals(true, test.sameStarChar("*"));
+		
+		assertEquals(true, test.sameStarChar("x*"));
+		assertEquals(true, test.sameStarChar("*x"));
+		
+		assertEquals(true, test.sameStarChar("biteme"));
+		
+		assertEquals(true, test.sameStarChar("xy*yzz"));
+		assertEquals(false, test.sameStarChar("xy*zzz"));
+		assertEquals(true, test.sameStarChar("*xa*az"));
+	}
+	
+	@Test
+	public void getSandwich(){
+		
+		assertEquals("", test.getSandwich(""));
+		assertEquals("", test.getSandwich("foobar"));
+		
+		assertEquals("", test.getSandwich("foobreadbar"));
+		assertEquals("", test.getSandwich("breadfoobar"));
+		assertEquals("", test.getSandwich("foobarbread"));
+		
+		assertEquals("jam", test.getSandwich("breadjambread"));
+		assertEquals("jam", test.getSandwich("xxbreadjambreadyy"));
+		
+	}
+	
+	@Test
+	public void xyzMiddle(){
+		assertEquals(false, test.xyzMiddle(""));
+		assertEquals(true, test.xyzMiddle("xyz"));
+		
+		assertEquals(true, test.xyzMiddle("xxyz"));
+		assertEquals(true, test.xyzMiddle("xyzx"));
+		
+		assertEquals(false, test.xyzMiddle("xy"));
+		assertEquals(false, test.xyzMiddle("a"));
+		assertEquals(false, test.xyzMiddle("ab"));
+		assertEquals(false, test.xyzMiddle("abc"));
+		assertEquals(false, test.xyzMiddle("abcd"));
+		assertEquals(false, test.xyzMiddle("abcde"));
+		
+		assertEquals(false, test.xyzMiddle("AAQQQBB"));
+		
+		assertEquals(true, test.xyzMiddle("AAxyzBB"));
+		assertEquals(true, test.xyzMiddle("AxyzBB"));
+		
+		assertEquals(true, test.xyzMiddle("xyzxyzAxyzBxyzxyz"));
+		assertEquals(true, test.xyzMiddle("xyzxyzxyzBxyzxyz"));
+	}
+	
+	@Test
+	public void prefixAgain(){
+		assertEquals(true, test.prefixAgain("abXYabc", 1));
+		assertEquals(true, test.prefixAgain("abXYabc", 2));
+		assertEquals(false, test.prefixAgain("abXYabc", 3));
+		
+	}
+	
+	@Test
+	public void repeatSeparator(){
+		assertEquals("WordXWordXWord", test.repeatSeparator("Word", "X", 3));
+		assertEquals("ThisAndThis", test.repeatSeparator("This", "And", 2));
+		assertEquals("This", test.repeatSeparator("This", "And", 1));
+	}
+	
+	@Test
+	public void repeatFront(){
+		assertEquals("", test.repeatFront("", 0));
+		assertEquals("a", test.repeatFront("a", 1));
+		
+		assertEquals("ChocChoChC", test.repeatFront("Chocolate", 4));
+		assertEquals("ChoChC", test.repeatFront("Chocolate", 3));
+		assertEquals("IcI", test.repeatFront("Ice Cream", 2));
+	}
+	
+	@Test
+	public void repeatEnd(){
+		assertEquals("", test.repeatEnd("", 1));
+		assertEquals("o", test.repeatEnd("Hello", 1));
+		assertEquals("lolo", test.repeatEnd("Hello", 2));
+		assertEquals("llollollo", test.repeatEnd("Hello", 3));
+	}
+	
+	
+	@Test
+	public void mixString(){
+		assertEquals("", test.mixString("", ""));
+		assertEquals("abc", test.mixString("abc", ""));
+		assertEquals("xyz", test.mixString("", "xyz"));
+		
+		assertEquals("HTihere", test.mixString("Hi", "There"));
+		
+		assertEquals("xTxhxexre", test.mixString("xxxx", "There"));
+	}
+	
+	
+	@Test
+	public void xyBalance(){
+		
+		assertEquals(true, test.xyBalance(""));
+		
+		assertEquals(true, test.xyBalance("y"));
+		assertEquals(false, test.xyBalance("x"));
+		assertEquals(true, test.xyBalance("z"));
+		
+		assertEquals(true, test.xyBalance("aaxbby"));
+		assertEquals(false, test.xyBalance("aaxbb"));
+		assertEquals(false, test.xyBalance("yaaxbb"));
+		assertEquals(true, test.xyBalance("yaaxbby"));
+	}
+	
+	
+	@Test
+	public void bobThere(){
+		assertEquals(true, test.bobThere("abcbob"));
+		assertEquals(true, test.bobThere("b9b"));
+		assertEquals(false, test.bobThere("bac"));
+		
+		assertEquals(false, test.bobThere("bb"));
+		assertEquals(true, test.bobThere("b1b"));
+	}
+	
+	@Test
+	public void xyzThere(){
+		assertEquals(true, test.xyzThere("abcxyz"));
+		assertEquals(false, test.xyzThere("abc.xyz"));
+		assertEquals(true, test.xyzThere("xyz.abc"));
+	}
+	
+	@Test
+	public void endOther(){
+		assertEquals(true, test.endOther("Hiabc", "abc"));
+		assertEquals(true, test.endOther("AbC", "HiaBc"));
+		assertEquals(true, test.endOther("abc", "abXabc"));
+		assertEquals(false, test.endOther("xxx", "aa"));
+		assertEquals(true, test.endOther("xxx", ""));
+	}
+	
+	@Test
 	public void countCode(){
 		assertEquals(1, test.countCode("aaacodebbb"));
 		assertEquals(2, test.countCode("codexxcode"));
